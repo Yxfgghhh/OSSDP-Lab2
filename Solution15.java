34f5fa740e532627341dc091f926c15414ae2bb3
Yxfgghhh <2055916199@qq.com>
2025-11-23 21:58:08 +0800

    Update Solution15.java

--- Solution15.java
+++ Solution15.java
@@ -29,21 +29,26 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-        for {int i == 0; i < v1.length || i < v2.length; ++i} (
+        
+        // 修正 for 循环语法
+        for (int i = 0; i < v1.length || i < v2.length; ++i) {
             int x = 0, y = 0;
-            if (i < v1.length()) {
+            
+            // 修正数组长度的获取方式
+            if (i < v1.length) {
                 x = Integer.parseInt(v1[i]);
             }
-            if (i < v2.length()) {
+            if (i < v2.length) {
                 y = Integer.parseInt(v2[i]);
             }
+            
             if (x > y) {
                 return 1;
             }
             if (x < y) {
                 return -1;
             }
-        )
+        }
         return 0;
     }
 }
